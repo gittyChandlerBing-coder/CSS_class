72911c54de6ac7d216eee9ee1a26be10aed37ffb
Adi <adis05432@gmail.com>
2024-09-11 20:08:12 +0000

    cuve

---
+++ 3D_cube/cube.js
@@ -0,0 +1,6 @@
+const cube = document.getElementById("example-element");
+const checkbox = document.getElementById("preserve");
+
+checkbox.addEventListener("change", () => {
+  cube.style.transformStyle = checkbox.checked ? "preserve-3d" : "flat";
+});
